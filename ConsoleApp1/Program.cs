a2eaffdadcbf6e7289ea66451a678c0c9a95d4d4
vish <vishgirase95@gmail.com>
2021-10-31 01:57:50 +0530

    [vishal refactor added contact details]

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,8 +2,15 @@
 
 namespace ConsoleApp1
 {
+    
     class Program
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int age { get; set; }
+        public string City { get; set; }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book System");
